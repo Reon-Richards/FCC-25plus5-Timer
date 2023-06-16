eed36cf8a21d0321e6fe690b824b21946ff4f052
Reon-Richards <rr7366@gmail.com>
2023-06-17 01:31:09 +0900

    added audio, updated App.js 25/29

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { useState } from "react";
-//import moment from 'moment';
 
 let running = false;
 let interval = null;
@@ -11,7 +10,6 @@ let displayMinutes = "25"
 let displaySeconds = "00"
 let sessionDisplay = "study";
 let firstStart = true;
-//let countTD = "25:00";
 
 function App() {
    
@@ -26,187 +24,213 @@ function App() {
     //const [seconds, setSeconds] = useState(59);
     //const [minutes, setMinutes] = useState(25);
        
-    function buttonStartPause() {
-      console.log(firstStart)
-      if ((running === false) && (breaktime === false) && (firstStart === true)){
+  function buttonStartPause() {
+        console.log(firstStart);
+    if ((running === false) && (breaktime === false) && (firstStart === true)){
         minutes = countStudyLength;};
-      if ((running === false) && (breaktime === true) && (firstStart === true)){
+    if ((running === false) && (breaktime === true) && (firstStart === true)){
         minutes = countBreakLength;
-      };
-        if (running){
-            clearInterval(interval);
-            //interval = null;
-            console.log(interval);
-            console.log(running);
-            running = false;
-            seconds = seconds;
-            console.log(seconds);
-            minutes = minutes;
-            console.log(minutes);
-      }
-        else {
-          interval = setInterval(startTimer, 1000);
-          console.log(interval);
-          console.log(running)
-          
+        };
+    if (running){
+        clearInterval(interval);
+        console.log(interval);
+        console.log(running);
+        running = false;
+        seconds = seconds;
+        console.log(seconds);
+        minutes = minutes;
+        console.log(minutes);
+        }
+
+    else {
+        interval = setInterval(startTimer, 1000);
+        console.log(interval);
+        console.log(running)
         };
     };
 
-    function startTimer () {
-      console.log(breaktime);
-      running = true;
-      firstStart = false;
-      console.log(running);
-      seconds -- ;
-      console.log(minutes);
-      console.log(seconds);
-      
-      if ((minutes === 0 && seconds === 0 && breaktime === false)){
-        clearInterval(interval);
-        studyTimerFinished();
+  function startTimer () {
+    console.log(breaktime);
+    running = true;
+    firstStart = false;
+    console.log(running);
+    seconds -- ;
+    console.log(minutes);
+    console.log(seconds);
+    
+    if ((minutes === 0 && seconds < 0 && breaktime === false)){
+      displayMinutes = "00";
+      displaySeconds = "00";
+      clearInterval(interval);
+      studyTimerFinished();
       };   
 
-      if ((minutes === 0 && seconds === 0 && breaktime === true)){
-        clearInterval(interval);
-        breakTimerFinished();
+    if ((minutes === 0 && seconds < 0 && breaktime === true)){
+      displayMinutes = "00";
+      displaySeconds = "00";
+      clearInterval(interval);
+      breakTimerFinished();
+      };
+      
+    if (seconds < 0){
+      displaySeconds = "00";
+      minutes -- ;
+      seconds = 59;
       };
 
-      if (seconds < 0){
-        minutes -- ;
-        seconds = 59;
+    if (minutes >= 10){
+      displayMinutes = minutes;
       };
 
-      if (minutes >= 10){
-        displayMinutes = minutes;
+    if (minutes < 1){
+      displayMinutes = "00"
       };
 
-      if (minutes < 1){
-        displayMinutes = "00"
+    if (minutes <= 9){
+      displayMinutes = "0" + minutes;
       };
 
-      if (minutes <= 9){
-        displayMinutes = "0" + minutes;
+    if (minutes < 1){
+      displayMinutes = "00"
       };
 
-      if (minutes < 1){
-        displayMinutes = "00"
+    if (seconds > 9){
+      displaySeconds = seconds;
       };
 
-      if (seconds > 9){
-        displaySeconds = seconds;
+    if(seconds <= 9){
+      displaySeconds = "0" + seconds;
       };
-
-      if(seconds <= 9){
-        displaySeconds = "0" + seconds;
-        };
-   
+  
     console.log(minutes);
     console.log(seconds);
     
     setCountTD ((displayMinutes)+":"+(displaySeconds));
+    console.log((displayMinutes)+":"+(displaySeconds));
     console.log((minutes)+":"+(seconds));
     console.log(countTD);
-    
     };
 
-    function studyTimerFinished(){
-      breaktime = true;
-      sessionDisplay = "break";
-      console.log("study timer has reached 00:00");
-      minutes = countBreakLength;
-      console.log(countBreakLength);
-      interval = setInterval(startTimer, 1000);
-      console.log(interval);
-      console.log(running);
-      };
-
-    function breakTimerFinished(){
-      breaktime = false;
-      sessionDisplay = "study";
-      console.log("break timer has finished")
-      minutes = countStudyLength;
-      console.log(countStudyLength);
-      interval = setInterval(startTimer, 1000);
-      console.log(interval);
-      console.log(running);
-
+  function studyTimerFinished(){
+    breaktime = true;
+    sessionDisplay = "break";
+    console.log("study timer has reached 00:00");
+    setCountTD("00:00");
+    playAlarm();
+    minutes = countBreakLength;
+    console.log(countBreakLength);
+    interval = setInterval(startTimer, 1000);
+    console.log(interval);
+    console.log(running);
     };
 
-    function buttonReset() {
-        clearInterval(interval);
-        console.log(interval);
-        seconds = 0;
-        minutes = 25;
-        running = false;
-        firstStart = true;
-        sessionDisplay = "study";
-        console.log(running)
-        setCountTD("25:00");
-        setCountStudyLength(25);
-        setCountBreakLength(5);
-        console.log(countTD);
-        console.log(countStudyLength);
-        console.log(countBreakLength);
-       };
+  function breakTimerFinished(){
+    breaktime = false;
+    sessionDisplay = "study";
+    console.log("break timer has finished")
+    setCountTD("00:00");
+    playAlarm();
+    minutes = countStudyLength;
+    console.log(countStudyLength);
+    interval = setInterval(startTimer, 1000);
+    console.log(interval);
+    console.log(running);
+    };
+
+  function playAlarm(){
+    let audio = document.getElementById("beep");
+    audio.play();
+    console.log(audio)
+  };
+
+  function resetAlarm(){
+    let audio = document.getElementById("beep");
+    audio.pause();
+    audio.currentTime = 0;
+  }
+
+  function buttonReset() {
+    clearInterval(interval);
+    console.log(interval);
+    resetAlarm();
+    seconds = 0;
+    minutes = 25;
+    running = false;
+    firstStart = true;
+    sessionDisplay = "study";
+    console.log(running)
+    setCountTD("25:00");
+    setCountStudyLength(25);
+    setCountBreakLength(5);
+    console.log(countTD);
+    console.log(countStudyLength);
+    console.log(countBreakLength);
+    };
   
-       function studyIncrement(){
-        if (firstStart === false){
-          return;
-        }
-        if (countStudyLength === 60){
-          return;
-        }
-        setCountStudyLength(countStudyLength +1);
-        console.log(countStudyLength);
-        minutes = countStudyLength;
-        console.log(minutes)
-      };  
+  function studyIncrement(){
+    if (firstStart === false){
+      return;
+    }
+
+    if (countStudyLength === 60){
+      return;
+    }
+
+    setCountStudyLength(countStudyLength +1);
+    console.log(countStudyLength);
+    minutes = countStudyLength;
+    console.log(minutes)
+    };  
       
-      function studyDecrement(){
-        if (firstStart === false){
-          return;
-        }
+  function studyDecrement(){
+    if (firstStart === false){
+      return;
+    }
 
-        if (countStudyLength === 1){
-          return;
-        }
-        setCountStudyLength(countStudyLength -1);
-        console.log(countStudyLength);
-        minutes = countStudyLength;
-        console.log(minutes)
-      };
+    if (countStudyLength === 1){
+      return;
+    }
 
-      function breakIncrement(){
-        if (firstStart === false){
-          return;
-        }
-        if (countBreakLength === 60){
-          return;
-        }
-        setCountBreakLength(countBreakLength +1)
-        console.log(countBreakLength)
-        minutes = countBreakLength;
-        console.log(minutes)
-        };
+    setCountStudyLength(countStudyLength -1);
+    console.log(countStudyLength);
+    minutes = countStudyLength;
+    console.log(minutes)
+    };
 
-      function breakDecrement(){
-        if (firstStart === false){
-          return;
-        }
+  function breakIncrement(){
+    if (firstStart === false){
+      return;
+    }
 
-        if (countBreakLength === 1){
-          return;
-        }
-        setCountBreakLength(countBreakLength -1);
-        console.log(countBreakLength)
-        minutes = countBreakLength;
-        console.log(minutes)
-        };
+    if (countBreakLength === 60){
+      return;
+    }
+
+    setCountBreakLength(countBreakLength +1)
+    console.log(countBreakLength)
+    minutes = countBreakLength;
+    console.log(minutes)
+    };
+
+  function breakDecrement(){
+    if (firstStart === false){
+      return;
+    }
+
+    if (countBreakLength === 1){
+      return;
+    }
+
+    setCountBreakLength(countBreakLength -1);
+    console.log(countBreakLength)
+    minutes = countBreakLength;
+    console.log(minutes)
+    };
 
   return (
     <div className="App">
         <div className = "timer">
-         <div className = "timeDisplay" id="time-left">{countTD}</div>
+          <div className = "timeDisplay" id="time-left">{countTD}</div>
           <div className = "startResetBtn"  id="start_stop"  onClick={() => buttonStartPause()}>START<br></br>PAUSE</div>
           <div className = "startResetBtn" id="reset" onClick={() => buttonReset()}>RESET</div>
           <hr id="hr1" ></hr>
@@ -225,8 +249,9 @@ function App() {
             <div className = "adjustBtn" id="break-decrement"  onClick={() => breakDecrement()}>-</div>
         </div>
       </div>
-    </div>
- );
+      <audio id="beep" src="https://assets.mixkit.co/active_storage/sfx/2576/2576-preview.mp3" ></audio>
+     </div>
+    );
 
 };
   
